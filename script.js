96e3f6330cd6ecd5a7be347a7d931a713e1d7fa5
Mansiv75 <mv072004@gmail.com>
2024-08-07 01:59:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const options = [
+    {id: "option1", text: "JavaScript", votes: 0},
+    {id: "option2", text: "Python", votes: 0},
+    {id: "option3", text: "Java", votes: 0},
+    {id: "option4", text: "C++", votes: 0}
+];
+
+function submitVote() {
+    const selectedOption = document.querySelector('input[name="poll"]:checked');
+    if (!selectedOption) {
+        alert('Please select an option.');
+        return;
+    }
+
+    const optionId = selectedOption.value;
+    const selectedOptionObj = options.find(option => option.id === optionId);
+    if (selectedOptionObj) {
+        selectedOptionObj.votes++;
+        displayResults();
+    }
+}
+
+function displayResults() {
+    const result = document.getElementById("result");
+    result.innerHTML = "";
+
+    options.forEach(option => {
+        const percentage = ((option.votes / getTotalVotes()) * 100).toFixed(2) || 0;
+        const barWidth = percentage > 0 ? percentage + "%" : "0%";
+
+        const optionResult = document.createElement("div");
+        optionResult.className = "option-result";
+
+        optionResult.innerHTML = `<span class='option-text'>${option.text}</span>`;
+        optionResult.innerHTML += `<div class='bar-container'><div class='bar' style="width:${barWidth};"></div></div>`;
+        optionResult.innerHTML += `<span class='percentage'>${percentage}%</span>`;
+
+        result.appendChild(optionResult);
+    });
+}
+
+function getTotalVotes() {
+    return options.reduce((total, option) => total + option.votes, 0);
+}
+
+// Initial display
+displayResults();
